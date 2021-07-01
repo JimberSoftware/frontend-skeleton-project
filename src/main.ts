d6b8cc1233837c0ab8e91622858e07e97861e894
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2021-07-01 11:12:42 +0200

    Updated locations of files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus';
 import App from './App.vue'
-import './index.css'
-import './assets/element-plus/element-plus.css'
+import './assets/tailwind/index.css'
+import './assets/element-plus/index.css'
 
 createApp(App).use(ElementPlus).mount('#app')
